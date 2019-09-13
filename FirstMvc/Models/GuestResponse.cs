eab4a6cf8ae41e3727a9c677c82b41ab628453c3
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-09-13 15:59:02 -0400

    Add project files.

---
+++ FirstMvc/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMvc.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phobne { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
