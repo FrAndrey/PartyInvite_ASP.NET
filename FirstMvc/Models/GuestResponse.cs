135333a79c06e9110acee0b5fd4c133ec9d3e532
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-09-14 12:29:50 -0400

    functionality added

--- FirstMvc/Models/GuestResponse.cs
+++ FirstMvc/Models/GuestResponse.cs
@@ -9,7 +9,7 @@ namespace FirstMvc.Models
     {
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Phobne { get; set; }
+        public string Phone { get; set; }
         public bool? WillAttend { get; set; }
     }
 }
